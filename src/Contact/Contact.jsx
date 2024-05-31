7407fcc3fd8c3f59676de0074f619d95558995a0
HelenYakovenko <elenus.light@gmail.com>
2024-05-31 23:18:49 +0300

    app contact

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,27 +1,19 @@
-import React from 'react';
+import css from "./Contact.module.css"
+import { FaUser, FaPhone } from "react-icons/fa6";
 
-// Компонент списку контактів
-const ContactList = ({ contacts, onRemoveContact }) => (
-  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <ul className="divide-y divide-gray-100">
-      {contacts.map(contact => (
-        <li className="flex justify-between gap-x-6 py-5" key={contact.id}>
-          {contact.name + ' : ' + contact.number}
-          {
-            // Кнопка видалення контакту
-            <button
-              class="flex w-24px justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              type="button"
-              name="delete"
-              onClick={() => onRemoveContact(contact.id)}
-            >
-              delete
-            </button>
-          }
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const Contact=({ data: { id, name, number }, onDelete })=> {
+    return (
+      <div className={css.container}>
+        <div className={css.text}>
+          <p><FaUser />   {name}</p>
 
-export default ContactList;
+          <p><FaPhone />   {number}</p>
+        </div>
+        <button className={css.btn} onClick={() => onDelete(id)}>
+          Delete
+        </button>
+      </div>
+    );
+}
+
+export default Contact;
